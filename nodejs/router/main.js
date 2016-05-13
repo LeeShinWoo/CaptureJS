c8771186e8c4ef944b06dd10d4ff47372171a964
dltlsdn1 <dltlsdn12312@nagosoft.com>
2016-05-13 15:38:09 +0900

    nodejs 추가 및 나머지 commit

---
+++ nodejs/router/main.js
@@ -0,0 +1,127 @@
+module.exports = function(app, fs)
+{
+    app.get('/',function(req,res){
+      sess = req.session;
+      res.render('index2', {
+             title: "MY HOMEPAGE",
+             length: 5,
+             name: sess.name,
+             username: sess.username
+         })
+
+    });
+    app.get('/about',function(req,res){
+      res.render('about.html');
+    });
+
+    app.get('/list', function (req, res) {
+      fs.readFile( __dirname + "/../data/" + "user.json", 'utf8', function (err, data) {
+          console.log( data );
+          res.end( data );
+      });
+   });
+
+   app.get('/getUser/:username', function(req, res){
+     fs.readFile( __dirname + "/../data/user.json", 'utf8', function (err, data) {
+          var users = JSON.parse(data);
+          // LOOP THRU USERS; SEARCH USERNAME
+          var result = {};
+          for(key in users){
+            if(key == req.params.username){
+                result["user_data"] = users[key];
+                break;
+            }
+      		}
+          res.json(result);
+     });
+  });
+
+  app.get('/login/:username/:password', function(req, res){
+        var sess;
+        sess = req.session;
+
+        fs.readFile(__dirname + "/../data/user.json", "utf8", function(err, data){
+            var users = JSON.parse(data);
+            var username = req.params.username;
+            var password = req.params.password;
+            var result = {};
+            if(!users[username]){
+                // USERNAME NOT FOUND
+                result["success"] = 0;
+                result["error"] = "not found";
+                res.json(result);
+                return;
+            }
+
+            if(users[username]["password"] == password){
+                result["success"] = 1;
+                sess.username = username;
+                sess.name = users[username]["name"];
+                console.log(sess.username);
+                console.log(sess.name);
+                res.json(result);
+
+            }else{
+                result["success"] = 0;
+                result["error"] = "incorrect";
+                res.json(result);
+            }
+        })
+    });
+
+
+    app.get('/logout', function(req, res){
+        sess = req.session;
+        if(sess.username){
+            req.session.destroy(function(err){
+                if(err){
+                    console.log(err);
+                }else{
+                    res.redirect('/');
+                }
+            })
+        }else{
+            res.redirect('/');
+        }
+    })
+
+
+  app.post('/addUser/:username', function(req, res){
+
+        var result = {};
+        var username = req.params.username;
+        console.log(req.headers);
+        console.log(req.params);
+        // CHECK REQ VALIDITY
+        if(!req.body["password"] || !req.body["name"]){
+            result["success"] = 0;
+            result["error"] = "invalid request";
+            res.json(result);
+            return;
+        }
+
+
+        // LOAD DATA & CHECK DUPLICATION
+        fs.readFile( __dirname + "/../data/user.json", 'utf8',  function(err, data){
+            var users = JSON.parse(data);
+            if(users[username]){
+                // DUPLICATION FOUND
+                result["success"] = 0;
+                result["error"] = "duplicate";
+                res.json(result);
+                return;
+            }
+
+            // ADD TO DATA
+            users[username] = req.body;
+            // SAVE DATA
+            fs.writeFile(__dirname + "/../data/user.json",
+                         JSON.stringify(users, null, '\t'), "utf8", function(err, data){
+                result = {"success": 1};
+                res.json(result);
+            })
+        })
+    });
+
+
+}
